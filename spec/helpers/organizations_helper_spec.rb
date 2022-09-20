5ebedf588a1b84aa56aa30d1fc8740d0a3c8245d
feierc <feierc@tamu.edu>
2022-09-19 21:23:35 -0500

    test cases commented

--- spec/helpers/organizations_helper_spec.rb
+++ spec/helpers/organizations_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe OrganizationsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe OrganizationsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -12,120 +12,120 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/organizations", type: :request do
+# RSpec.describe "/organizations", type: :request do
   
-  # This should return the minimal set of attributes required to create a valid
-  # Organization. As you add validations to Organization, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+#   # This should return the minimal set of attributes required to create a valid
+#   # Organization. As you add validations to Organization, be sure to
+#   # adjust the attributes here as well.
+#   let(:valid_attributes) {
+#     skip("Add a hash of attributes valid for your model")
+#   }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+#   let(:invalid_attributes) {
+#     skip("Add a hash of attributes invalid for your model")
+#   }
 
-  describe "GET /index" do
-    it "renders a successful response" do
-      Organization.create! valid_attributes
-      get organizations_url
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /index" do
+  #   it "renders a successful response" do
+  #     Organization.create! valid_attributes
+  #     get organizations_url
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "GET /show" do
-    it "renders a successful response" do
-      organization = Organization.create! valid_attributes
-      get organization_url(organization)
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /show" do
+  #   it "renders a successful response" do
+  #     organization = Organization.create! valid_attributes
+  #     get organization_url(organization)
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_organization_url
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /new" do
+  #   it "renders a successful response" do
+  #     get new_organization_url
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "GET /edit" do
-    it "renders a successful response" do
-      organization = Organization.create! valid_attributes
-      get edit_organization_url(organization)
-      expect(response).to be_successful
-    end
-  end
+  # describe "GET /edit" do
+  #   it "renders a successful response" do
+  #     organization = Organization.create! valid_attributes
+  #     get edit_organization_url(organization)
+  #     expect(response).to be_successful
+  #   end
+  # end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Organization" do
-        expect {
-          post organizations_url, params: { organization: valid_attributes }
-        }.to change(Organization, :count).by(1)
-      end
+  # describe "POST /create" do
+  #   context "with valid parameters" do
+  #     it "creates a new Organization" do
+  #       expect {
+  #         post organizations_url, params: { organization: valid_attributes }
+  #       }.to change(Organization, :count).by(1)
+  #     end
 
-      it "redirects to the created organization" do
-        post organizations_url, params: { organization: valid_attributes }
-        expect(response).to redirect_to(organization_url(Organization.last))
-      end
-    end
+  #     it "redirects to the created organization" do
+  #       post organizations_url, params: { organization: valid_attributes }
+  #       expect(response).to redirect_to(organization_url(Organization.last))
+  #     end
+  #   end
 
-    context "with invalid parameters" do
-      it "does not create a new Organization" do
-        expect {
-          post organizations_url, params: { organization: invalid_attributes }
-        }.to change(Organization, :count).by(0)
-      end
+  #   context "with invalid parameters" do
+  #     it "does not create a new Organization" do
+  #       expect {
+  #         post organizations_url, params: { organization: invalid_attributes }
+  #       }.to change(Organization, :count).by(0)
+  #     end
 
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post organizations_url, params: { organization: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
+  #     it "renders a successful response (i.e. to display the 'new' template)" do
+  #       post organizations_url, params: { organization: invalid_attributes }
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  # describe "PATCH /update" do
+  #   context "with valid parameters" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
 
-      it "updates the requested organization" do
-        organization = Organization.create! valid_attributes
-        patch organization_url(organization), params: { organization: new_attributes }
-        organization.reload
-        skip("Add assertions for updated state")
-      end
+  #     it "updates the requested organization" do
+  #       organization = Organization.create! valid_attributes
+  #       patch organization_url(organization), params: { organization: new_attributes }
+  #       organization.reload
+  #       skip("Add assertions for updated state")
+  #     end
 
-      it "redirects to the organization" do
-        organization = Organization.create! valid_attributes
-        patch organization_url(organization), params: { organization: new_attributes }
-        organization.reload
-        expect(response).to redirect_to(organization_url(organization))
-      end
-    end
+  #     it "redirects to the organization" do
+  #       organization = Organization.create! valid_attributes
+  #       patch organization_url(organization), params: { organization: new_attributes }
+  #       organization.reload
+  #       expect(response).to redirect_to(organization_url(organization))
+  #     end
+  #   end
 
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        organization = Organization.create! valid_attributes
-        patch organization_url(organization), params: { organization: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
+  #   context "with invalid parameters" do
+  #     it "renders a successful response (i.e. to display the 'edit' template)" do
+  #       organization = Organization.create! valid_attributes
+  #       patch organization_url(organization), params: { organization: invalid_attributes }
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested organization" do
-      organization = Organization.create! valid_attributes
-      expect {
-        delete organization_url(organization)
-      }.to change(Organization, :count).by(-1)
-    end
+#   describe "DELETE /destroy" do
+#     it "destroys the requested organization" do
+#       organization = Organization.create! valid_attributes
+#       expect {
+#         delete organization_url(organization)
+#       }.to change(Organization, :count).by(-1)
+#     end
 
-    it "redirects to the organizations list" do
-      organization = Organization.create! valid_attributes
-      delete organization_url(organization)
-      expect(response).to redirect_to(organizations_url)
-    end
-  end
-end
+#     it "redirects to the organizations list" do
+#       organization = Organization.create! valid_attributes
+#       delete organization_url(organization)
+#       expect(response).to redirect_to(organizations_url)
+#     end
+#   end
+# end
