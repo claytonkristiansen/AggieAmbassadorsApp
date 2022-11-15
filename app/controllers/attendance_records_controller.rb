cde3484ca241a13835603d186c9e7191b894bb0c
Clayton Kristiansen <kristiansenc@DESKTOP-3DV10QD.localdomain>
2022-11-14 23:35:15 -0600

    AMBASSADOR-25 Added attendance record show page

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -1,9 +1,28 @@
 class AttendanceRecordsController < ApplicationController
     def index
-        @attendance_record = AttendanceRecord.all
+        @attendance_records = AttendanceRecord.all
     end
 
     def show; end
+
+    def delete
+        @attendance_record = AttendanceRecord.find(params[:id])
+    end
+
+    # Attendance Record controller method that actually deletes attendance record item from database
+    def destroy
+        @attendance_record = AttendanceRecord.find(params[:id])
+        @attendance_record.destroy
+
+        respond_to do |format|
+            format.html { redirect_to(attendance_records_url, notice: 'Attendance Record was successfully destroyed.') }
+            format.json { head(:no_content) }
+        end
+    end
+
+    def edit;
+
+    end
 
     def new
         @attendance_record = AttendanceRecord.new
