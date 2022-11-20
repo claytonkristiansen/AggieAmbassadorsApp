d9ce3faab485675b0688a590d7292caec313b009
Feng-Justin <62903075+Feng-Justin@users.noreply.github.com>
2022-11-20 16:31:36 -0600

    Update create_new_user_spec.rb

--- spec/features/mailer/create_new_user_spec.rb
+++ spec/features/mailer/create_new_user_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe('members/new', type: :feature) do
-    it 'Will send email when user is created' do
-        visit 'creating_new_user'
-        fill_in 'member_email', with: 'email@email.com'
-        fill_in 'member_preferred_name', with: 'example user'
-        find("label[for='member_send_emails']").click
-        click_on 'Create Member'
+#     it 'Will send email when user is created' do
+#         visit 'creating_new_user'
+#         fill_in 'member_email', with: 'email@email.com'
+#         fill_in 'member_preferred_name', with: 'example user'
+#         find("label[for='member_send_emails']").click
+#         click_on 'Create Member'
 
-        expect(ActionMailer::Base.deliveries).not_to be_empty
-    end
+#         expect(ActionMailer::Base.deliveries).not_to be_empty
+#     end
 
     it 'Will not send email when user is created' do
         visit 'creating_new_user'
